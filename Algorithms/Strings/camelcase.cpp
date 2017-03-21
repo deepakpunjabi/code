d73263a12cc05c176fc8e573386b4c262a49f105
Deepak Punjabi <deepakpunjabi13@gmail.com>
2017-03-21 13:57:20 +0530

    added camelcase

---
+++ Algorithms/Strings/camelcase.cpp
@@ -0,0 +1,39 @@
+// https://www.hackerrank.com/challenges/camelcase
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main() {
+	string s;
+	cin >> s;
+	int c = 1;
+	for (int i = 1; i < s.length(); i++) {
+		if (s[i] >= 65 && s[i] < 97)
+			c++;
+	}
+	cout << c;
+	return 0;
+}
