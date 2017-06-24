b7f458cd58a340d8d7aa15e4214f87b63285f7de
Deepak Punjabi <deepakpunjabi13@gmail.com>
2017-06-25 00:12:47 +0530

    Luck Balance
    https://www.hackerrank.com/challenges/luck-balance

---
+++ Algorithms/Greedy/luck-balance.cpp
@@ -0,0 +1,54 @@
+// https://www.hackerrank.com/challenges/luck-balance
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+void printPairVec(vector<pair<int, int>> arr)
+{
+    for (auto i : arr)
+    {
+        cout << i.first << " " << i.second << "\t";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n, k, i;
+    cin >> n >> k;
+    int luck, imp;
+    int luckGained = 0;
+
+    vector<pair<int, int>> luckImp;
+
+    for (i = 0; i < n; ++i)
+    {
+        cin >> luck >> imp;
+        luckImp.emplace_back(luck, imp);
+    }
+
+    sort(begin(luckImp), end(luckImp), [](pair<int, int> a, pair<int, int> b) {
+        return a.first > b.first;
+    });
+
+    // printPairVec(luckImp);
+
+    for (auto i : luckImp)
+    {
+        if (k <= 0 && i.second == 1)
+        {
+            luckGained -= i.first;
+            continue;
+        }
+        if (i.second == 1)
+        {
+            --k;
+        }
+        luckGained += i.first;
+    }
+
+    cout << luckGained << endl;
+    return 0;
+}
