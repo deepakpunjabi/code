b0fb5fe474c30a3c53e0837c227f09c048a80625
deepakpunjabi <deepakpunjabi13@gmail.com>
2019-05-09 00:01:57 +0530

    added searching in rotated array + guess number

--- Algorithms/binary_search.cpp
+++ Algorithms/binary_search.cpp
@@ -67,4 +67,51 @@ int mySqrt(int x) {
   return left - 1;
 }
 
+class RotatedSortedArray {
+public:
+    int search(vector<int>& nums, int target) {       
+      return searchBinary(nums, 0, nums.size()-1, target);
+    }
+    
+    int searchBinary(vector<int>& nums, int left, int right, int target) {
+      if (left > right) {
+        return -1;
+      }
+      int mid = left + (right-left)/2;
+      if (nums[mid] == target) {
+        return mid;
+      } 
+      if (int res = searchBinary(nums, left, mid-1, target); res != -1) {
+        return res;
+      }
+      return searchBinary(nums, mid+1, right, target);
+    }
+};
+
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class GuessNumber {
+public:
+    int guessNumber(int n) {
+      int left = 1, right=n; 
+      
+      while (true) {
+        int num = left + (right-left)/2;
+        int res = guess(num);
+        
+        if (!res) {
+          return num;
+        } else if (res > 0) {
+          left = num + 1;
+        } else {
+          right = num -1;
+        }
+      }
+    
+    }
+};
+
 int main() { cout << mySqrt(4); }
