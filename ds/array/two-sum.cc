f3a3f38d90443bdf7ef3641ceb91e1033de5e6dc
deepakpunjabi <deepakpunjabi13@gmail.com>
2019-09-15 15:50:55 +0530

    added two sum

---
+++ ds/array/two-sum.cc
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/two-sum
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = m.find(target - nums[i]);
+            if (it != m.end()) {
+                return vector<int>{it->second, i};
+            } else {
+                m.emplace(nums[i], i);
+            }
+        }
+        // redundant
+        return vector<int>{};
+    }
+};
