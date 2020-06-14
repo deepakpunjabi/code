e9eab35f06cfe21552606cd33891f656adf1f376
deepakpunjabi <deepakpunjabi13@gmail.com>
2020-06-14 10:48:28 +0530

    Added sorting main example

--- algo/sorting/main.cc
+++ algo/sorting/main.cc
@@ -2,28 +2,24 @@
 
 using namespace std;
 
-void printArray(int a[], int n)
-{
-    for(int i=0; i<n; ++i)
-    {
+void printArray(int a[], int n) {
+    for (int i = 0; i < n; ++i) {
         cout << a[i] << " ";
     }
 }
 
-int main()
-{
+int main() {
     int n, x;
     cin >> n >> x;
     int i;
     int a[n];
-    
-    for(i=0; i<n; ++i)
-    {
+
+    for (i = 0; i < n; ++i) {
         cin >> a[i];
     }
-    
+
     //selectionSort(a, n, x);
     printArray(a, n);
-    
+
     return 0;
 }
