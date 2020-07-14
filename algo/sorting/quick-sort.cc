4ef88e76909bfada81789fa7d520e37b0a30c024
deepakpunjabi <deepakpunjabi13@gmail.com>
2020-07-14 14:15:20 +0530

    Updated quick sort

--- algo/sorting/quick-sort.cc
+++ algo/sorting/quick-sort.cc
@@ -1,4 +1,4 @@
-#include <utility>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,6 +7,7 @@ void quickSort(int arr[], int start, int end) {
         return;
     }
 
+    // get partition index, and divide main problem into subproblems
     int pi = partition(arr, start, end);
     quickSort(arr, start, pi - 1);
     quickSort(arr, pi + 1, end);
@@ -16,12 +17,27 @@ int partition(int arr[], int start, int end) {
     int pivot = arr[end];
     int pi = start;
 
+    while (start < end) {
+        while (arr[start] <= pivot) {
+            ++start;
+        }
+        while (arr[end] > pivot) {
+            --end;
+        }
+        swap(arr[start], arr[end]);
+    }
+    
+    /* 
     for (int i = start; i < end; ++i) {
         if (arr[i] <= pivot) {
             swap(arr[i], arr[pi]);
             ++pi;
         }
     }
+    */
+    
+    // end is now has last start value, which is less than pivot,
+    // hence we need to put end to the left of pivot
     swap(arr[pi], arr[end]);
     return pi;
 }
