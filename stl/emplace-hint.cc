005b3ce01dcba6bcbba63d373353671d3d44c6a6
deepakpunjabi <deepakpunjabi13@gmail.com>
2020-08-12 19:47:19 +0530

    updated emplace hint

--- stl/emplace-hint.cc
+++ stl/emplace-hint.cc
@@ -4,16 +4,18 @@
 using namespace std;
 
 int main() {
-    multimap<int,int> mm = {{1,1}};
+    multimap<int, int> mm = {{1, 1}};
 
     // gives lower_bound iterator for element in multi map
     // useful for finding all elements with same key in multimap
     auto it = mm.lower_bound(1);
-    
+
     // hint is used for efficient insertion
     // insert algorithm will start its search from hint iterator
     // useful for continous insertion
-    mm.emplace_hint(it, pair<int, int>(2,2));
+
+    auto p = make_pair(2, 2);
+    mm.emplace_hint(it, p);
 
     cout << mm.size() << endl;
 }
