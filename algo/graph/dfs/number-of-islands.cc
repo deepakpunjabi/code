10ccc49ec540015153466624bfc9dc71be2d60b5
deepakpunjabi <deepakpunjabi13@gmail.com>
2020-08-12 12:30:19 +0530

    Added number of islands

---
+++ algo/graph/dfs/number-of-islands.cc
@@ -0,0 +1,66 @@
+/*
+
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+*/
+
+#include <vector>
+
+using namespace std;
+
+/*
+    - traverse the grid
+    - whenever you find 1, mark all adjacent 1's recursively and add 1 to the result
+    - repeat
+*/
+class Islands {
+   public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.size() == 0) return 0;
+
+        int islands = 0;
+
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    ++islands;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size()) return;
+        if (grid[i][j] != '1') return;
+
+        grid[i][j] = '2';
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+    }
+};
