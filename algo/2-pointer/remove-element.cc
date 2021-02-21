a9b07bebf3680b4a22f855b9b475b99bd3a77195
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-02-21 22:45:03 +0530

    Create remove-element.cc

---
+++ algo/2-pointer/remove-element.cc
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+    if (nums.empty()) return 0;
+
+    int start = 0;
+    int end = nums.size() - 1;
+    while (start <= end) {
+        if (nums[start] == val) {
+            swap(nums[start], nums[end]);
+            --end;
+        }
+        // only move start after a successfull swap
+        // eg [3,3] val=3 shouldn't increament start
+        if (nums[start] != val) {
+            ++start;
+        }
+    }
+
+    return start;
+}
