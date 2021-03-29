69eb79e1b8f05f3082763a7a0f799b7a20ac8360
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-03-29 21:19:04 +0530

    Update move-zeroes.cc

--- misc/move-zeroes.cc
+++ misc/move-zeroes.cc
@@ -12,6 +12,40 @@ void moveZeroes(vector<int>& nums) {
     }
 }
 
+// without swap!
+void moveZeroes(vector<int>& nums) {
+    int start = 0;
+
+    // just put the damn non-zero elements in the start
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i] != 0) {
+            nums[start++] = nums[i];
+        }
+    }
+
+    // set rest of the elements to zero
+    for (int i = start; i < nums.size(); ++i) {
+        nums[i] = 0;
+    }
+}
+
+// complicated but understandable logic
+void moveZeroes(vector<int>& nums) {
+    if (nums.empty()) return;
+
+    int pos = 0;
+    for (int i = 1; i < nums.size() and pos < nums.size(); ++i) {
+        // swap when pos is 0 but i is not, pos < i is imp!
+        if (pos < i && nums[pos] == 0 && nums[i] != 0) {
+            swap(nums[pos], nums[i]);
+        }
+        // increament pos while you can
+        while (pos < nums.size() && nums[pos] != 0) {
+            ++pos;
+        }
+    }
+}
+
 /*
 
 Given an array nums, write a function to move all 0's to the end of it 
