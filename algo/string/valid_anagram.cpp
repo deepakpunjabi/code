3615770260c2866f99b538143708674f6d734c80
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-03-31 22:37:22 +0530

    Update valid_anagram.cpp

--- algo/string/valid_anagram.cpp
+++ algo/string/valid_anagram.cpp
@@ -1,47 +1,44 @@
+/*
+
+  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+  Example 1:
+  Input: s = "anagram", t = "nagaram"
+  Output: true
+
+  Example 2:
+  Input: s = "rat", t = "car"
+  Output: false
+  
+  Constraints:
+  1 <= s.length, t.length <= 5 * 104
+  s and t consist of lowercase English letters.
+  
+  Follow up: What if the inputs contain Unicode characters? 
+  How would you adapt your solution to such a case?
+
+*/
+
 #include <string>
 #include <unordered_map>
-
 using namespace std;
 
 class Solution {
- public:
-  bool isAnagram(string s, string t) {
-    if (s.size() != t.size()) {
-      return false;
-    }
+   public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
 
-    int index[26] = {};
-    for (int i = 0; i < s.size(); ++i) {
-      index[s[i] - 'a']++;
-      index[t[i] - 'a']--;
+        // may as well use an unordered_map or a vector
+        int index[26] = {};
+        for (int i = 0; i < s.size(); ++i) {
+            index[s[i] - 'a']++;
+            index[t[i] - 'a']--;
+        }
+        for (int i : index) {
+            if (index[i]) return false;
+        }
+        return true;
     }
-    for (int i : index) {
-      if (index[i]) return false;
-    }
-    return true;
-  }
 };
-
-bool isPalindrome(string s) {
-  for (int i = 0, j = s.size() - 1; i < j;) {
-    if (!isalnum(s[i])) {
-      ++i;
-      continue;
-    }
-    if (!isalnum(s[j])) {
-      --j;
-      continue;
-    }
-
-    if (tolower(s[i]) != tolower(s[j])) {
-      return false;
-    }
-    ++i, --j;
-  }
-  return true;
-}
-
-void test() {
-    unordered_map<int, int> mmap;
-    
-}
