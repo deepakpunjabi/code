e463975bffe0776ac04e684d9287b4754fcd72b8
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-05-23 22:11:36 +0530

    Update index-equals-value.cc

--- daily-byte/index-equals-value.cc
+++ daily-byte/index-equals-value.cc
@@ -30,12 +30,13 @@ int indexEqualsValue(vector<int> &nums) {
 
 int main() {
     vector<vector<int>> tc = {
-        {},
-        {0, 4, 8},
-        {1, 2, 3},     // mutliple output possible, go FCFS
-        {1, 3, 4, 6},  // -1
+        {},               // empty array: -1
+        {1, 1, 2, 3},     // multiple possible results, go fcfs ie 1
+        {0, 4, 8},        // 0
+        {1, 2, 3},        // -1
+        {1, 3, 4, 6},     // -1
         {1, 2, 4, 4, 4},  // duplicate values but sorted
-        };           // empty array
+    };
 
     for (auto &i : tc) {
         cout << indexEqualsValue(i) << endl;
