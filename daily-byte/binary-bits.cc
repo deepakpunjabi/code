b027d0e5e8629cba3f48f3ab870bf57ef2c48865
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-05-27 17:04:44 +0530

    Update binary-bits.cc

--- daily-byte/binary-bits.cc
+++ daily-byte/binary-bits.cc
@@ -16,20 +16,53 @@
 #include <map>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 using namespace std;
+
+int binaryBits(vector<int> &nums) {
+    if (nums.empty()) return 0;
+
+    int res = 0;
+    int currmax = 0;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i]) {
+            ++currmax;
+        } else {
+            res = max(res, currmax);
+            currmax = 0;
+        }
+    }
+
+    return max(res, currmax);
+}
 
 int main() {
     vector<vector<int>> tc = {
         {},
-        {0, 4, 8},
-        {1, 2, 3},        // mutliple output possible, go FCFS
-        {1, 3, 4, 6},     // -1
-        {1, 2, 4, 4, 4},  // duplicate values but sorted
-    };                    // empty array
+        
+        {0},
+        {1},
+
+        {0, 0},
+        {0, 1},
+        {1, 0},
+        {1, 1},
+        
+        {0, 0, 1},
+        {1, 0, 1},
+
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {1, 1, 1, 1},
+        
+        {0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
+        
+    };
 
     for (auto &i : tc) {
-        // cout << indexEqualsValue(i) << endl;
+        cout << i.size() << " --> " << binaryBits(i) << endl;
     }
 
     return 0;
