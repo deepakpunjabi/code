c9bb82cdfcc202366ea4af26fbe87111b3638b6f
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-05-27 17:44:57 +0530

    Update longest-substring-between-characters.cc

--- daily-byte/longest-substring-between-characters.cc
+++ daily-byte/longest-substring-between-characters.cc
@@ -4,10 +4,9 @@
     Note: s will only contain lowercase alphabetical characters.
 
     Ex: Given the following string s…
-
     s = "bbccb", return 3 ("bcc" is length 3).
+    
     Ex: Given the following string s…
-
     s = "abb", return 0.
 
 */
@@ -20,17 +19,47 @@
 #include <vector>
 using namespace std;
 
+int longestSubstr(string s) {
+    if (s.length() < 3) return 0;
+
+    int longest = 0;
+    for (int i = 0; i < s.length() - 2; ++i) {  // because only length 3+ can have substr
+        for (int j = s.length() - 1; j > i; --j) {
+            if (s[i] == s[j]) {
+                longest = max(longest, j - i - 1);
+            }
+        }
+    }
+
+    return longest;
+}
+
 int main() {
-    vector<vector<int>> tc = {
-        {},
-        {0, 4, 8},
-        {1, 2, 3},        // mutliple output possible, go FCFS
-        {1, 3, 4, 6},     // -1
-        {1, 2, 4, 4, 4},  // duplicate values but sorted
-    };                    // empty array
+    vector<string> tc = {
+        "",  // 0
+
+        "a",  // 0
+
+        "ab",  // 0
+        "aa",  // 0
+
+        "abb",  // 0
+        "aba",  // 1 - b
+        "aaa",  // 1 - a
+
+        "abca",  // 2 - bc
+        "abba",  // 2 - bb
+        "abab",  // 1 - a
+
+        "abcde",  // 0
+        "abcbc",  // 1 - c,b
+        "abccb",  // 2 - cc
+        "abcca",  // 3 - bcc
+
+    };
 
     for (auto &i : tc) {
-        // cout << indexEqualsValue(i) << endl;
+        cout << i << " --> " << longestSubstr(i) << endl;
     }
 
     return 0;
