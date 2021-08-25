05b6551275e10f7564978da15f22c2828860d35d
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-08-25 09:04:26 +0530

    Update single-character-substring.cc

--- daily-byte/single-character-substring.cc
+++ daily-byte/single-character-substring.cc
@@ -20,14 +20,30 @@
 #include <vector>
 using namespace std;
 
+int singleCharacterSubstring(string s) {
+    if (s.empty()) return 0;
+
+    int count = 1;
+    int res = 0;
+
+    for (int i = 1; i < s.length(); ++i) {
+        if (s[i] == s[i - 1]) {
+            ++count;
+        } else {
+            res += count * (count + 1) / 2;
+            count = 1;
+        }
+    }
+}
+
 int main() {
     vector<vector<int>> tc = {
         {},
         {0, 4, 8},
-        {1, 2, 3},        // mutliple output possible, go FCFS
-        {1, 3, 4, 6},     // -1
-        {1, 2, 4, 4, 4},  // duplicate values but sorted
-    };                    // empty array
+        {1, 2, 3},
+        {1, 3, 4, 6},
+        {1, 2, 4, 4, 4},
+    };
 
     for (auto &i : tc) {
         // cout << indexEqualsValue(i) << endl;
