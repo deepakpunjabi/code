c6ddceddc6b463a0d5e8da4c1d05efaab4d6c30e
deepakpunjabi <deepakpunjabi13@gmail.com>
2021-08-28 22:36:39 +0530

    Update disjoint_set.cc

--- ds/disjoint_set.cc
+++ ds/disjoint_set.cc
@@ -3,42 +3,59 @@
 using namespace std;
 
 class DisjointSet {
-    int *arr;
-    int *size;
+    int *root;  // store root for each node
+    int *rank;  // store size of each subtree
+
+    // vector<int> root;
+    // vector<int> rank;
+
+    // DisjointSet(int size) : root(size), rank(size) {
+    //     for (int i = 0; i < size; ++i) {
+    //         root[i] = i;
+    //         rank[i] = 1;
+    //     }
+    // }
 
     DisjointSet(int n) {
-        arr = new int[n];
-        size = new int[n];
+        root = new int[n];
+        rank = new int[n];
         for (int i = 0; i < n; ++i) {
-            arr[i] = i;
-            size[i] = 1;
+            root[i] = i;
+            rank[i] = 1;
         }
     }
+
     ~DisjointSet() {
-        delete arr;
-        delete size;
+        delete root;
+        delete rank;
     }
 
+    // path-compression algorithm
     int find(int x) {
-        if (arr[x] != x) {
-            arr[x] = find(arr[x]);
+        if (root[x] == x) {
+            return x;
         }
-        return arr[x];
+        root[x] = find(root[x]);
+        return root[x];
     }
 
-    void unionBySize(int x, int y) {
-        int r1 = find(x);
-        int r2 = find(y);
+    void unionByrank(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
 
-        if (r1 == r2) return;
+        if (rx == ry) return;
 
-        if (size[r1] < size[r2]) {
-            swap(r1, r2);
+        if (rank[rx] < rank[ry]) {
+            swap(rx, ry);
         }
 
-        arr[r2] = r1;
-        if (size[r1] == size[r2]) {
-            ++size[r1];
+        root[ry] = rx;
+        if (rank[rx] == rank[ry]) {
+            ++rank[rx];
         }
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
     }
 };
