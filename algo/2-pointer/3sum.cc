019fabfb67985f596befb87d05c59e74a2d11199
Deepak Punjabi <deepakpunjabi13@gmail.com>
2022-04-13 00:35:20 +0530

    updated 3sum

--- algo/2-pointer/3sum.cc
+++ algo/2-pointer/3sum.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <sstream>
@@ -13,6 +14,7 @@ vector<vector<int>> threeSum(vector<int>& nums) {
     sort(nums.begin(), nums.end());
 
     for (int i = 0; i < nums.size() - 2; ++i) {
+        // skip duplicates
         if (i > 0 && nums[i] == nums[i - 1]) continue;
 
         int target = 0 - nums[i];
@@ -57,64 +59,18 @@ vector<int> twoSum(vector<int>& nums, int low, int high, int target) {
     return vector<int>{};
 }
 
-// ----------------------------------------------------
-// bool resExists(vector<vector<int>>& m, vector<int>& r);
-
-bool resExists(unordered_set<string>& res, vector<int>& r) {
-    stringstream ss;
-    ss << r[0] << r[1] << r[2];
-
-    string s = ss.str();
-    if (res.find(s) != res.end()) {
-        return true;
-    }
-    res.insert(s);
-    return false;
-}
-
-vector<vector<int>> threeSumTLE(vector<int>& nums) {
-    vector<vector<int>> res;
-    map<int, vector<int>> elem;
-    unordered_set<string> e;
-
-    for (int i = 0; i < nums.size(); ++i) {
-        auto j = elem.find(nums[i]);
-        if (j == elem.end()) {
-            elem[nums[i]] = vector<int>(0);
+void print(vector<vector<int>>& res) {
+    for (const auto& i : res) {
+        for (const auto& j : i) {
+            cout << j << " ";
         }
-        elem[nums[i]].push_back(i);
+        cout << endl;
     }
-
-    for (int i = 0; i < nums.size(); ++i) {
-        for (int j = i + 1; j < nums.size(); ++j) {
-            int tmp = nums[i] + nums[j];
-
-            const auto& it = elem.find(0 - tmp);
-            if (it != elem.end()) {
-                for (auto k : it->second) {
-                    if (k != i && k != j) {
-                        vector<int> a = {nums[i], nums[j], nums[k]};
-                        sort(begin(a), end(a));
-                        if (resExists(e, a)) continue;
-                        res.push_back(a);
-                    }
-                }
-            }
-        }
-    }
-
-    return res;
 }
 
 int main() {
     // vector<int> nums = {-1, 0, 1, 2, -1, -4};
     vector<int> nums = {0};
     vector<vector<int>> res = threeSum(nums);
-    for (const auto& i : res) {
-        cout << "whoop" << endl;
-        for (const auto& j : i) {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
+    print(res);
 }
