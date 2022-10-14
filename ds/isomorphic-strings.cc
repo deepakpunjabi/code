803e75722190985bacafeed765d198591d303ab6
Deepak Punjabi <deepakpunjabi13@gmail.com>
2022-10-14 14:17:52 +0530

    Update isomorphic-strings.cc

--- ds/isomorphic-strings.cc
+++ ds/isomorphic-strings.cc
@@ -2,7 +2,7 @@
 
     Given two strings s and t, determine if they are isomorphic.
     Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-    All occurrences of a character must be replaced with another character while preserving the order of characters. 
+    All occurrences of a character must be replaced with another character while preserving the order of characters.
     No two characters may map to the same character, but a character may map to itself.
 
     Example 1:
@@ -16,7 +16,7 @@
     Example 3:
     Input: s = "paper", t = "title"
     Output: true
-    
+
     Constraints:
     1 <= s.length <= 5 * 104
     t.length == s.length
@@ -29,6 +29,26 @@
 #include <vector>
 
 using namespace std;
+
+bool isIsomorphic(string s, string t) {
+    if (s.size() != t.size()) return false;
+
+    unordered_map<char, char> isomorph;
+    unordered_map<char, char> isomorphOf;
+    for (int i = 0; i < s.size(); ++i) {
+        if (isomorph.find(s[i]) == isomorph.end() && isomorphOf.find(t[i]) == isomorphOf.end()) {
+            isomorph[s[i]] = t[i];
+            isomorphOf[t[i]] = s[i];
+        } else if (
+            (isomorph.find(s[i]) != isomorph.end() && isomorphOf.find(t[i]) == isomorphOf.end()) || (isomorph.find(s[i]) == isomorph.end() && isomorphOf.find(t[i]) != isomorphOf.end())) {
+            return false;
+        } else if (isomorph[s[i]] != t[i] || isomorphOf[t[i]] != s[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 // here isomorphic means 1-1 mapping
 // hence we need to check both ways
@@ -71,7 +91,7 @@ bool isIsomorphic(string s, string t) {
     for (size_t i = 0; i < s.size(); i++) {
         // if both counts are diff then one of them has been part of extra mapping
         if (m1[s[i]] != m2[t[i]]) return false;
-        
+
         m1[s[i]] = i + 1;
         m2[t[i]] = i + 1;
     }
