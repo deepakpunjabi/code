06ab4c828120aa64fd0acffad629c5f78b42b862
Deepak Punjabi <deepakpunjabi13@gmail.com>
2022-11-03 23:09:59 +0530

    Update decode-string.cc

--- ds/stack/decode-string.cc
+++ ds/stack/decode-string.cc
@@ -1,15 +1,15 @@
-/* 
+/*
 Given an encoded string, return its decoded string.
-The encoding rule is: k[encoded_string], 
-where the encoded_string inside the square brackets is being repeated exactly k times. 
+The encoding rule is: k[encoded_string],
+where the encoded_string inside the square brackets is being repeated exactly k times.
 Note that k is guaranteed to be a positive integer.
 
 You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
 
-Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k.
 For example, there won't be input like 3a or 2[4].
 
- 
+
 
 Example 1:
 Input: s = "3[a]2[bc]"
@@ -28,12 +28,13 @@ Input: s = "abc3[cd]xyz"
 Output: "abccdcdcdxyz"
 */
 
+#include <iostream>
 #include <stack>
 #include <string>
 
 using namespace std;
 
-string decodeString(string s) {
+string decodeStringOld(string s) {
     stack<int> counts;
     stack<string> str;
 
@@ -75,4 +76,51 @@ string decodeString(string s) {
     }
 
     return curr;
+}
+
+string decodeString(string s) {
+    stack<string> st;
+
+    string curr = "";
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '[') {
+            st.push(curr);
+            curr = "";
+        } else if (s[i] == ']') {
+            string top1 = st.top();
+            cout << top1 << endl;
+            int num = stoi(top1);
+            st.pop();
+
+            string tmp = "";
+            while (num--) {
+                tmp += curr;
+            }
+            st.push(tmp);
+            curr = "";
+        } else {
+            if ((isalpha(curr[0]) && isalpha(s[i])) or (isdigit(curr[0]) && isdigit(s[i]))) {
+                curr += s[i];
+            } else {
+                st.push(curr);
+                curr = s[i];
+            }
+                
+        }
+        cout << i << " --> " << curr << endl;
+    }
+
+    string res = "";
+    while (!st.empty()) {
+        res = st.top() + res;
+        st.pop();
+    }
+
+    return res;
+}
+
+int main() {
+    string t1 = "3[a]2[bc]";
+    string t2 = "3[a2[c]]";
+    cout << decodeString(t2) << endl;
 }
